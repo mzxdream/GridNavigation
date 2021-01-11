8dafd4e3282d440fab15691771ae002cebaa2b45
mengzhaoxiang <mengzhaoxiang@xd.com>
2021-01-11 14:50:06 +0800

    1

--- Assets/Scripts/Board/GameBoard.cs
+++ Assets/Scripts/Board/GameBoard.cs
@@ -1,14 +1,54 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class GameBoard : MonoBehaviour
 {
     [SerializeField]
+    Vector2Int size = new Vector2Int(11, 11);
+    [SerializeField]
     Transform ground = default;
     [SerializeField]
-    Vector2Int size;
-    public void Init()
+    Texture2D gridTexture = default;
+    [SerializeField]
+    GameTile tilePrefab = default;
+    [SerializeField]
+    GameTileContentFactory tileContentFactory = default;
+    GameTile[] tiles;
+
+    public bool Init()
     {
+        ground.localScale = new Vector3(size.x, size.y, 1f);
+        var material = ground.GetComponent<MeshRenderer>().material;
+        material.mainTexture = gridTexture;
+        material.SetTextureScale("_MainTex", size);
+
+        var offset = new Vector2((size.x - 1) * 0.5f, (size.y - 1) * 0.5f);
+        tiles = new GameTile[size.x * size.y];
+        for (int y = 0; y < size.y; y++)
+        {
+            for (int x = 0; x < size.x; x++)
+            {
+                var tile = Instantiate(tilePrefab);
+                tile.transform.SetParent(transform, false);
+                tile.transform.localPosition = new Vector3(x - offset.x, 0, y - offset.y);
+                tile.Content = tileContentFactory.Get(GameTileContentType.Wall);
+                tiles[x + y * size.x] = tile;
+            }
+        }
+        return true;
+    }
+    public void Clear()
+    {
+    }
+
+    void OnValidate()
+    {
+        if (size.x < 2)
+        {
+            size.x = 2;
+        }
+        if (size.y < 2)
+        {
+            size.y = 2;
+        }
     }
 }
--- Assets/Scripts/Board/GameTile.cs
+++ Assets/Scripts/Board/GameTile.cs
@@ -2,4 +2,19 @@ using UnityEngine;
 
 public class GameTile : MonoBehaviour
 {
+    GameTileContent content;
+    public GameTileContent Content
+    {
+        get => content;
+        set
+        {
+            Debug.Assert(value != null, "Null assigned to content!");
+            if (content != null)
+            {
+                content.Recycle();
+            }
+            content = value;
+            content.transform.localPosition = transform.localPosition;
+        }
+    }
 }
