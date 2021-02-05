d7c29a843d859c0f142652b25cc2985cf2cff999
mzxdream <mzxdream@gmail.com>
2021-02-05 16:06:34 +0800

    1

--- Assets/Scripts/Logic/GridMoveAgent.cs
+++ Assets/Scripts/Logic/GridMoveAgent.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class GridMoveAgentParams
+public class GridMoveAgentParam
 {
     public int teamID; //enemy can't push
     public int unitSize;
@@ -11,13 +11,31 @@ public class GridMoveAgentParams
 
 public class GridMoveAgent
 {
-    private int unitSize = 3; //odd number and >= 3
-    private Vector3 pos = Vector3.zero;
+    GridMoveManager manager;
+    private int id;
+    private Vector3 pos;
+    private Vector3 forward;
+    GridMoveAgentParam param;
+    private int gridIndex;
 
-    public GridMoveAgent()
+    public GridMoveAgent(GridMoveManager manager)
+    {
+        this.manager = manager;
+    }
+    public bool Init(int id, Vector3 pos, Vector3 forward, GridMoveAgentParam param)
+    {
+        this.id = id;
+        this.pos = pos;
+        this.forward = forward;
+        this.param = param;
+
+        manager.ClampInBounds(ref pos);
+        this.gridIndex = manager.GetGridIndex(pos);
+        return true;
+    }
+    public void Clear()
     {
     }
-
     public void Update()
     {
     }
--- Assets/Scripts/Logic/GridMoveManager.cs
+++ Assets/Scripts/Logic/GridMoveManager.cs
@@ -18,14 +18,15 @@ public class GridMoveManager
     }
 
     private int gameSpeed = 30;
-    private Vector3 pos = Vector3.zero;
+    private Vector3 bmin = Vector3.zero;
+    private Vector3 bmax = Vector3.zero;
     private int gridX = 11;
     private int gridZ = 11;
     private float gridSize = 0.2f;
 
     private int frameNum = 0;
     private Grid[] grids;
-    private GridMoveAgent[] agents;
+    private List<GridMoveAgent> agents; //TODO optimization
     private GridPathFinder pathFinder;
 
     public GridMoveManager()
@@ -34,7 +35,9 @@ public class GridMoveManager
     public bool Init(int gameSpeed, Vector3 pos, int gridX, int gridZ, float gridSize, int maxAgents)
     {
         this.gameSpeed = gameSpeed;
-        this.pos = pos;
+        var offset = new Vector3(gridX * gridSize / 2, 0, gridZ * gridSize / 2);
+        this.bmin = pos - offset;
+        this.bmax = pos + offset;
         this.gridX = gridX;
         this.gridZ = gridZ;
         this.gridSize = gridSize;
@@ -48,20 +51,53 @@ public class GridMoveManager
                 this.grids[x + z * gridX] = new Grid(x, z);
             }
         }
-        this.agents = new GridMoveAgent[maxAgents];
+        this.agents = new List<GridMoveAgent>();
         this.pathFinder = new GridPathFinder(gridX, gridZ);
         return true;
     }
     public void Clear()
     {
     }
+    public GridMoveAgent CreateAgent(Vector3 pos, Vector3 forward, GridMoveAgentParam agentParam)
+    {
+        GridMoveAgent agent = new GridMoveAgent(this);
+        if (!agent.Init(agents.Count + 1, pos, forward, agentParam))
+        {
+            return null;
+        }
+        agents.Add(agent);
+        return agent;
+    }
+    public void ClampInBounds(ref Vector3 pos)
+    {
+        pos.x = Mathf.Clamp(pos.x, bmin.x, bmax.x);
+        pos.z = Mathf.Clamp(pos.z, bmin.z, bmax.z);
+    }
+    public int GetGridIndex(Vector3 pos)
+    {
+        int x = (int)((pos.x - bmin.x) / gridSize);
+        int z = (int)((pos.z - bmin.z) / gridSize);
+        return Mathf.Clamp(x, 0, gridX - 1) + Mathf.Clamp(z, 0, gridZ - 1) * gridX;
+    }
+    public Vector3 GetGridPos(int index)
+    {
+        Debug.Assert(index >= 0 && index < gridX * gridZ);
+        int z = index / gridX;
+        int x = index - z * gridX;
+        return new Vector3(bmin.x + (x + 0.5f) * gridSize, 0, bmin.z + (z + 0.5f) * gridSize);
+    }
     public void Update()
     {
-
+        foreach (var agent in agents)
+        {
+            agent.Update();
+        }
     }
-
     public void LateUpdate()
     {
-
+        foreach (var agent in agents)
+        {
+            agent.LateUpdate();
+        }
     }
 }
