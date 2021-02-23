26c04c5993e2db2b7d29eb2c13b792c5f0a920e3
mzxdream <mzxdream@gmail.com>
2021-02-23 12:27:55 +0800

    1

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -83,7 +83,7 @@ public class Game : MonoBehaviour
                     prefab = redCharacterPrefab;
                     break;
                 case CharacterType.Blue:
-                    prefab = Instantiate(blueCharacterPrefab);
+                    prefab = blueCharacterPrefab;
                     break;
                 default:
                     Debug.Assert(false, "unsupported character type:" + type);
