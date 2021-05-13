93c81c993513b20aafcc912e7c1e0f0d670f463e
mzxdream <mzxdream@gmail.com>
2021-05-13 17:05:09 +0800

    1

--- Assets/Scripts/Logic/GridNavMath.cs
+++ Assets/Scripts/Logic/GridNavMath.cs
@@ -1,10 +1,10 @@
 using UnityEngine;
 
 public enum GridNavDirection { None = 0, Left = 1, Right = 2, Up = 3, Down = 4, LeftUp = 5, RightUp = 6, LeftDown = 7, RightDown = 8 }
-public enum GridNavDirectionOpt { None = 0, Left = 1, Right = 2, Up = 4, Down = 8 }
 
 public static class GridNavMath
 {
+    private enum GridNavDirectionOpt { None = 0, Left = 1, Right = 2, Up = 4, Down = 8 }
     private static readonly GridNavDirection[] optToDirs = {
         GridNavDirection.None, GridNavDirection.Left,     GridNavDirection.Right,     GridNavDirection.None,
         GridNavDirection.Up,   GridNavDirection.LeftUp,   GridNavDirection.RightUp,   GridNavDirection.None,
@@ -17,23 +17,15 @@ public static class GridNavMath
         GridNavDirectionOpt.Left | GridNavDirectionOpt.Down, GridNavDirectionOpt.Right | GridNavDirectionOpt.Down
     };
 
+    public static GridNavDirection CombineDirection(GridNavDirection dir1, GridNavDirection dir2)
+    {
+        return optToDirs[(int)(dirToOpts[(int)dir1] | dirToOpts[(int)dir2])];
+    }
     public static float DistanceApproximately(int sx, int sz, int ex, int ez)
     {
         int dx = Mathf.Abs(ex - sx);
         int dz = Mathf.Abs(ez - sz);
         return (dx + dz) + Mathf.Min(dx, dz) * (1.4142f - 2.0f);
-    }
-    public static GridNavDirection ToDirection(GridNavDirectionOpt opt)
-    {
-        return optToDirs[(int)opt];
-    }
-    public static GridNavDirectionOpt ToDirectionOpt(GridNavDirection dir)
-    {
-        return dirToOpts[(int)dir];
-    }
-    public static GridNavDirection CombineDirection(GridNavDirection dir1, GridNavDirection dir2)
-    {
-        return optToDirs[(int)(dirToOpts[(int)dir1] | dirToOpts[(int)dir2])];
     }
     public static float SqrDistance2D(Vector3 a, Vector3 b)
     {
