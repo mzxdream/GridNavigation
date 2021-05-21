6e6ada02b30bbf4953afe86c69cf093cbf83612e
mzxdream <mzxdream@gmail.com>
2021-05-21 18:21:08 +0800

    123

--- Assets/Scripts/Logic/GridNavManager.cs
+++ Assets/Scripts/Logic/GridNavManager.cs
@@ -197,6 +197,38 @@ namespace GridNav
         }
         public void UpdateMoveRequest()
         {
+            int maxNodes = 10240;
+            while (moveRequestQueue.Count > 0 && maxNodes > 0) //寻路
+            {
+                var agent = agents[moveRequestQueue[0]];
+                if (agent.moveState == NavMoveState.Requesting)
+                {
+                    agent.moveState = NavMoveState.WaitForPath;
+                    var circleIndex = navMesh.GetSquareCenterIndex(agent.squareIndex, agent.goalSquareIndex);
+                    var circleRadius = navMesh.DistanceApproximately(agent.squareIndex, circleIndex) * 3.0f + 100.0f;
+                    var constraint = new GridNavQueryConstraintCircle(agent.goalSquareIndex, agent.goalRadius, circleIndex, circleRadius);
+                    pathRequestNavQuery.InitSlicedFindPath(agent.pathFilter, agent.squareIndex, constraint);
+                }
+                if (agent.state == GridNavAgentState.WaitForPath)
+                {
+                    Debug.Assert(pathRequestQueue[0] == agent.id);
+                    var status = pathRequestNavQuery.UpdateSlicedFindPath(maxNodes, out var doneNodes);
+                    maxNodes -= doneNodes;
+                    if (status != GridNavQueryStatus.InProgress)
+                    {
+                        pathRequestQueue.RemoveAt(0);
+                        if (status == GridNavQueryStatus.Failed)
+                        {
+                            agent.state = GridNavAgentState.None;
+                        }
+                        else if (status == GridNavQueryStatus.Success)
+                        {
+                            agent.state = GridNavAgentState.Moving;
+                            pathRequestNavQuery.FinalizeSlicedFindPath(out agent.path);
+                        }
+                    }
+                }
+            }
         }
     }
 }
