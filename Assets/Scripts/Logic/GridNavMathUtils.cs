7713b567a6fac507222c8859c091fafbe7b5ca0f
mzxdream <mzxdream@gmail.com>
2021-05-28 18:02:06 +0800

    1231

--- Assets/Scripts/Logic/GridNavMathUtils.cs
+++ Assets/Scripts/Logic/GridNavMathUtils.cs
@@ -23,12 +23,20 @@ namespace GridNav
         public static Vector3 Normalized2D(Vector3 a)
         {
             var mag = (a.x * a.x + a.z * a.z);
-            if (mag < 1e-6f)
+            if (mag < EPSILON)
             {
                 return Vector3.zero;
             }
             mag = Mathf.Sqrt(mag);
             return new Vector3(a.x / mag, 0.0f, a.z / mag);
+        }
+        public static float Dot2D(Vector3 a, Vector3 b)
+        {
+            return a.x * b.x + a.z * b.z;
+        }
+        public static float Det2D(Vector3 a, Vector3 b)
+        {
+            return a.x * b.z - a.z * b.x;
         }
         public static float LeftOf2D(Vector3 a, Vector3 b, Vector3 c)
         {
@@ -39,7 +47,7 @@ namespace GridNav
             var x1 = a.x;
             var z1 = a.z;
             var d = Mathf.Sqrt(x1 * x1 + z1 * z1);
-            if (d > Mathf.Epsilon)
+            if (d > EPSILON)
             {
                 x1 /= d;
                 z1 /= d;
@@ -52,7 +60,7 @@ namespace GridNav
             var x2 = b.x;
             var z2 = b.z;
             d = Mathf.Sqrt(x2 * x2 + z2 * z2);
-            if (d > Mathf.Epsilon)
+            if (d > EPSILON)
             {
                 x2 /= d;
                 z2 /= d;
@@ -87,14 +95,6 @@ namespace GridNav
                 return to;
             }
             return from.x * to.z - from.z * to.x > 0 ? Rotate2D(from, -angle) : Rotate2D(from, angle);
-        }
-        public static float Dot2D(Vector3 a, Vector3 b)
-        {
-            return a.x * b.x + a.z * b.z;
-        }
-        public static float Det2D(Vector3 a, Vector3 b)
-        {
-            return a.x * b.z - a.z * b.x;
         }
         public static bool ClosestHeightPointTriangle(Vector3 p, Vector3 a, Vector3 b, Vector3 c, out float h)
         {
