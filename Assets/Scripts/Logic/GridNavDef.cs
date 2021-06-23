4921a0ef99237663edc861b5a10153345e8fe84d
mzxdream <mzxdream@gmail.com>
2021-06-23 11:30:29 +0800

    123

--- Assets/Scripts/Logic/GridNavDef.cs
+++ Assets/Scripts/Logic/GridNavDef.cs
@@ -16,20 +16,60 @@ namespace GridNav
     public class NavMoveDef
     {
         // speed = speedMod * speedModMult / (1.0f + slope * slopeMod)
-        public int unitSize; // >=4的偶数
-        public float maxSlope = 0.5f; // 最大爬坡角度(0.0f - 1.0f) => (0 - 90) cos曲线(1-cos60*)
-        public float slopeMod = 0.0f; // 爬坡消耗,值越大，消耗越大 
-        public float[] speedMods = new float[NavDef.MaxAreaTypes]; // <=0表示不能行走,值越大速度越快
-        public float[] speedModMults = new float[(int)NavSpeedModMultType.NumTypes]; // 对应NavSpeedModMultType 值越小，寻路消耗越大
+        private int unitSize = 4; // >=4的偶数
+        private float maxSlope = 0.5f; // 最大爬坡角度(0.0f - 1.0f) => (0 - 90) cos曲线(1-cos60*)
+        private float slopeMod = 0.0f; // 爬坡消耗,值越大，消耗越大 
+        private float[] speedMods = new float[NavDef.MaxAreaTypes]; // <=0表示不能行走,值越大速度越快
+        private float[] speedModMults = new float[(int)NavSpeedModMultType.NumTypes]; // 对应NavSpeedModMultType 值越小，寻路消耗越大
 
         public NavMoveDef()
         {
-            unitSize = 4;
             speedMods[0] = 1.0f;
             speedMods[1] = 0.0f;
             speedModMults[(int)NavSpeedModMultType.Idle] = 0.35f;
             speedModMults[(int)NavSpeedModMultType.Busy] = 0.10f;
             speedModMults[(int)NavSpeedModMultType.Moving] = 0.65f;
+        }
+        public void SetUnitSize(int unitSize)
+        {
+            Debug.Assert(unitSize >= 4 && (unitSize & 1) == 0);
+            this.unitSize = unitSize;
+        }
+        public int GetUnitSize()
+        {
+            return unitSize;
+        }
+        public void SetMaxSlope(float maxSlope)
+        {
+            Debug.Assert(maxSlope >= 0.0f && maxSlope <= 1.0f);
+            this.maxSlope = maxSlope;
+        }
+        public float GetMaxSlope()
+        {
+            return maxSlope;
+        }
+        public void SetSlopeMod(float slopeMod)
+        {
+            this.slopeMod = slopeMod;
+        }
+        public float GetSlopeMod()
+        {
+            return slopeMod;
+        }
+        public void SetSpeedMod(int areaType, float speedMod)
+        {
+            Debug.Assert(areaType >= 0 && areaType < speedMods.Length);
+            speedMods[areaType] = speedMod;
+        }
+        public float GetSpeedMod(int areaType)
+        {
+            Debug.Assert(areaType >= 0 && areaType < speedMods.Length);
+            return speedMods[areaType];
+        }
+        public void SetSpeedModMult(NavSpeedModMultType type, float speedModMult)
+        {
+            Debug.Assert((int)type >= 0 && (int)type < speedModMults.Length);
+            speedModMults[(int)type] = speedModMult;
         }
     }
 
@@ -55,7 +95,6 @@ namespace GridNav
         public float goalRadius;
         public List<Vector3> path;
         public Vector3 velocity;
-        public float speed;
         public Vector3 prefVelocity;
         public Vector3 newVelocity;
         public bool isMoving;
